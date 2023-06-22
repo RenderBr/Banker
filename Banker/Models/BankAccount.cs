309167418982e1fdb8c9d22da78273be42ccaf6d
average <julianmuchacho666@gmail.com>
2023-06-21 20:43:11 -0400

    tidied up BankAccount models

--- Banker/Models/BankAccount.cs
+++ Banker/Models/BankAccount.cs
@@ -4,15 +4,15 @@ namespace Banker.Models
 {
 	public interface IBankAccount
 	{
-		public string AccountName { get; set; }
+		string AccountName { get; set; }
 
-		public float Currency { get; set; }
+		float Currency { get; set; }
 
+		string JointAccount { get; set; }
 
-		public string JointAccount { get; set; }
-
-		public bool IsInJointAccount() => !string.IsNullOrEmpty(JointAccount);
+		bool IsInJointAccount() => !string.IsNullOrWhiteSpace(JointAccount);
 	}
+
 
 	public class LinkedBankAccount : LinkedModel, IBankAccount
 	{
@@ -24,24 +24,21 @@ namespace Banker.Models
 
 		public string _jointAccount = string.Empty;
 
-		public string JointAccount
-		{
-			get => _jointAccount; set { _ = this.SaveAsync(x => x.JointAccount, value); _jointAccount = value; }
+		public string JointAccount { get => _jointAccount; set { _ = this.SaveAsync(x => x.JointAccount, value); _jointAccount = value; } }
 
-		}
+	}
 
-		public class BankAccount : BsonModel, IBankAccount
-		{
-			private string _accountName = string.Empty;
-			public string AccountName { get => _accountName; set { _ = this.SaveAsync(x => x.AccountName, value); _accountName = value; } }
+	public class BankAccount : BsonModel, IBankAccount
+	{
+		private string _accountName = string.Empty;
+		public string AccountName { get => _accountName; set { _ = this.SaveAsync(x => x.AccountName, value); _accountName = value; } }
 
-			private float _currency;
-			public float Currency { get => _currency; set { _ = this.SaveAsync(x => x.Currency, value); _currency = value; } }
+		private float _currency;
+		public float Currency { get => _currency; set { _ = this.SaveAsync(x => x.Currency, value); _currency = value; } }
 
-			public string _jointAccount = string.Empty;
+		public string _jointAccount = string.Empty;
 
-			public string JointAccount { get => _jointAccount; set { _ = this.SaveAsync(x => x.JointAccount, value); _jointAccount = value; } }
+		public string JointAccount { get => _jointAccount; set { _ = this.SaveAsync(x => x.JointAccount, value); _jointAccount = value; } }
 
-		}
 	}
 }
